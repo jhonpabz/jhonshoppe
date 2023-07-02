36aa0678a673367a7dc33b2d6b05327316c700d2
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-07-02 20:27:56 +0800

    Added new layout for product page.

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,13 +1,18 @@
 <script setup>
+definePageMeta({
+  layout: false,
+});
 const { id } = useRoute().params;
 const { data: product, pending } = await useGetProduct(id);
 console.log(pending.value, 'pending');
 </script>
 
 <template>
-  <Head>
-    <Title>JHONSHOPPE | {{ product.title }}</Title>
-    <Meta name="description" :content="product.description" />
-  </Head>
-  <ProductDetails :product="product" />
+  <NuxtLayout name="custom">
+    <Head>
+      <Title>JHONSHOPPE | {{ product.title }}</Title>
+      <Meta name="description" :content="product.description" />
+    </Head>
+    <ProductDetails :product="product" />
+  </NuxtLayout>
 </template>
